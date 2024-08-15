e02ae172905ee480ee4f22f34f8b3158074273d7
YaAlan1 <chill.alanus@gmail.com>
2024-08-15 11:10:05 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+let month = +prompt("Введите номер месяца")
+switch (month) {
+    case 1:
+        alert("Январь-31 день")
+    break;
+
+    case 2:
+        alert("Февраль-29 день")
+    break;
+
+    case 3:
+        alert("Март-31 день")
+    break;
+
+    case 4:
+        alert("Апрель-30 день")
+    break;
+
+    case 5:
+        alert("Май-31 день")
+    break;
+
+    case 6:
+        alert("Июнь-30 день")
+    break;
+
+    case 7:
+        alert("Июль-31 день")
+    break;
+
+    case 8:
+        alert("Август-31 день")
+    break;
+
+    case 9:
+        alert("Сентябрь-30 день")
+    break;
+
+    case 10:
+        alert("Октябрь-31 день")
+    break;
+
+    case 11:
+        alert("Ноябрь-30 день")
+    break;
+
+    case 12:
+        alert("Декабрь-31 день")
+    break;
+
+    default:
+        alert("Такого месяца нету")
+        break;
+}
